5a378aa93f5327744be40eef6b61e67fc1d518c4
Alex Drummond <a.d.drummond@gmail.com>
2017-07-12 01:01:13 +0100

    Add code for parsing commmand line args.

--- src/Main.hs
+++ src/Main.hs
@@ -7,10 +7,26 @@ import Control.Exception (throw)
 import Network.HTTP.Req (responseBody)
 import Control.Applicative ((<|>))
 import System.Environment (getArgs, getEnv)
+import qualified Text.Read as TR
 
 main :: IO ()
 main = do
+  (filename, batchSize) <- (parseArgs <$> getArgs) >>= liftEither
   apiKey <- (T.pack <$> getEnv "OCTOPART_API_KEY")
             <|> fail "You must define the environment variable OCTOPART_API_KEY before running this program"
   json <- responseBody <$> O.queryMpns apiKey [T.pack "SN74S74N"]
   print json
+
+-- Command line syntax is too simple to make it worthwhile to use a command
+-- line parsing library.
+parseArgs :: [String] -> Either String (String, Int)
+parseArgs args
+  | length args /= 2 = Left usage
+  | True = case TR.reads (args !! 1) of
+             [(n,"")] -> Right ((args !! 0), n)
+             _ -> Left usage
+
+usage = "Program must be given two arguments: filename and batch size (integer)"
+
+liftEither :: Either String a -> IO a
+liftEither = either fail return
