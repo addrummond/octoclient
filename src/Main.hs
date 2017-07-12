2bc0e150b9d57a97246991350f62f6e56814729c
Alex Drummond <a.d.drummond@gmail.com>
2017-07-12 13:02:35 +0100

    Add main program logic to Main.hs; appears to work correctly.

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,21 @@ import qualified CSV
 import qualified Octopart as O
 import qualified Data.Text as T
 import qualified Data.ByteString as B
+import qualified Data.Vector as V
+import qualified Data.Vector.Algorithms.Merge as VA
+import Data.Scientific
 import Control.Exception (throw)
 import Network.HTTP.Req (responseBody)
 import Control.Applicative ((<|>))
 import System.Environment (getArgs, getEnv)
 import qualified Text.Read as TR
 import qualified BOM
+
+-- Octopart API docs state that at most 20 queries may be made in a single request.
+-- This program uses the smallest number of requests possible given this constraint.
+octopart_max_queries = 20
+
+usage = "Program must be given two arguments: filename and batch size (integer >= 1)"
 
 main :: IO ()
 main = do
@@ -18,8 +27,34 @@ main = do
             <|> fail "You must define the environment variable OCTOPART_API_KEY before running this program"
   csvContents <- B.readFile filename
   bomLines <- liftEither $ BOM.fromCsv csvContents
-  json <- responseBody <$> O.queryMpns apiKey [T.pack "SN74S74N"]
-  print json
+  partNumbers <- return $ map BOM.partNumber bomLines
+  queries <- return $ groupN octopart_max_queries partNumbers
+  -- At this point we convert the vector of vectors of query responses
+  -- to a list of query responses, since we're going to loop through
+  -- without consing. The alternative would be to V.concatMap using
+  -- the vector representation, which would definitely require allocating
+  -- a new vector.
+  responses <- concatMap (V.toList . O.unwrapResponses . responseBody)
+               <$> mapM (O.queryMpns apiKey) queries
+  bestPrices <-
+    return $ map (\(quantity, offers) -> (bestTotalPrice (quantity * batchSize) offers)
+                                         * (fromIntegral quantity) * (fromIntegral batchSize))
+                 (zip (map BOM.quantity bomLines) responses)
+  print $ sum bestPrices
+
+bestTotalPrice :: Int -> V.Vector O.Offer -> Scientific
+bestTotalPrice n offers =
+  V.foldl' f (0 :: Scientific) offers
+    where
+      f best offer
+        | O.quantity offer <= n && (best == 0 || O.priceUSD offer < best) = O.priceUSD offer
+        | True = best
+
+-- Given an integer i, splits a list l into the smallest list of sublists l'
+-- such that (a) concat l' == l and (b) each member of l' is of length <= i.
+groupN :: Int -> [a] -> [[a]]
+groupN _ [ ] = [ ]
+groupN i l = firstN : (groupN i rest) where (firstN, rest) = splitAt i l
 
 -- Command line syntax is too simple to make it worthwhile to use a command
 -- line parsing library.
@@ -32,14 +67,6 @@ parseArgs args
                  then Left usage
                  else Right ((args !! 0), n)
              _ -> Left usage
-
-usage = "Program must be given two arguments: filename and batch size (integer >= 1)"
-
--- Given an integer i, splits a list l into the smallest list of sublists l'
--- such that (a) concat l' == l and (b) each member of l' is of length <= i.
-groupN :: Int -> [a] -> [[a]]
-groupN _ [ ] = [ ]
-groupN i l = firstN : (groupN i rest) where (firstN, rest) = splitAt i l
 
 liftEither :: Either String a -> IO a
 liftEither = either fail return
